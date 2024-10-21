d2ca091f77f937d10735cedd101b841dcab78258
sumanta dutta <pminstu@imptobnet.com>
2024-10-21 20:48:31 +0200

    Transform Module Integrated

--- framework/base/base.py
+++ framework/base/base.py
@@ -1,15 +1,33 @@
 from __future__ import annotations
 
 from pydantic import BaseModel, Field
-from typing import Optional, Dict
 from abc import ABC, abstractmethod
+from pyspark.sql import DataFrame
+from typing import Optional, Any
 
 
 class BaseReader(BaseModel):
     full_path: str = Field(..., description="full path to the file with filename")
     format: str = Field(..., description="format of the file to be read")
 
+    @abstractmethod
+    def read(self):
+        raise NotImplementedError
 
-    # @abstractmethod
-    # def reader(self):
-    #     raise NotImplementedError
+
+class BaseWriter(BaseModel):
+    full_path: str = Field(..., description="full path to the file with filename")
+    format: str = Field(..., description="format of the file to be write")
+    mode: Optional[str] = Field("overwrite", description="whether to overwrite or append")
+    delimiter: Optional[str] = Field(",", description="delimited to be used")
+
+    @abstractmethod
+    def write(self):
+        raise NotImplementedError
+
+
+class BaseTransform(BaseModel):
+    df: Optional[Any] = Field(..., description="source dataframe")
+    @abstractmethod
+    def transform(self, **kwargs):
+        raise NotImplementedError
---
+++ framework/transform/option1.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from framework.base.base import BaseTransform
+from pydantic import Field, validator
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as f
+from typing import Optional, Any
+
+
+class TransformOption1(BaseTransform):
+    @validator("df", allow_reuse=True, always=True, check_fields=False)
+    def dataframe_validator(cls, df):
+        assert isinstance(
+            df, DataFrame
+        ), f"Expecting pyspark.sql.DataFrame type, received {type(df)}"
+        return df
+
+    def transform(self, df2: DataFrame):
+        df1 = self.df.filter(f.col("area") == "IT")
+        join_df = df1.join(df2, on=["id"], how="inner").orderBy(f.col("sales_amount").desc())
+
+        return join_df
+
+
