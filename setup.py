5115050b60350f232a49e6974555f0dd6c73257d
sumanta dutta <pminstu@imptobnet.com>
2024-10-23 21:52:32 +0200

    Commit setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="cingest",
+    version="0.0.1",
+    packages=[
+        "framework",
+        "framework.base",
+        "framework.reader",
+        "framework.transform",
+        "framework.writer",
+    ],
+    url="",
+    license="",
+    author="Sumanta Dutta",
+    author_email="sumanta.dutta2012@gmail.com",
+    description="ABN Amrp technical assignment package",
+)
