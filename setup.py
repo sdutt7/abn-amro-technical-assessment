21ebf630240a9eca38e6874a85d5e455771c94a9
sdutt7 <sumanta.dutta2012@gmail.com>
2024-10-23 22:08:13 +0200

    Feature/it data (#2)
    * CSV Reader Module Integrated
    
    * Transform Module Integrated
    
    * Writer Module Integrated
    
    * Option2 transformation integrated
    
    * Option2 transformation integrated
    
    * Option2 transformation integrated
    
    * Option3 transformation integrated
    
    * Option4 transformation integrated
    
    * Option4 transformation integrated
    
    * Option6 transformation integrated
    
    * Fixed bug
    
    * Fixed bug
    
    * Fixed bug
    
    * Transform7 Integrated
    
    * Transform7 Integrated
    
    * Transform8 Integrated
    
    * Job created
    
    * Job created
    
    * Job created
    Output file added
    
    * transform1 test case added
    
    * transform2 test case added
    
    * transform2 test case added
    
    * transform3 test case added
    
    * transform3 test case added
    
    * transform4 test case added
    
    * transform5 test case added
    
    * transform6 test case added
    
    * Docstring added
    
    * Docstring added
    
    * Output files committed
    
    * Commit setup.py
    
    * Commit setup.py
    
    * Commit setup.py
    
    * Commit setup.py
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    ---------
    
    Co-authored-by: sumanta dutta <pminstu@imptobnet.com>

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="abn_amro_test",
+    version="0.0.1",
+    packages=[
+        "framework",
+        "framework.base",
+        "framework.reader",
+        "framework.transform",
+        "framework.writer",
+    ],
+    url="",
+    license="",
+    install_requires=required,
+    author="Sumanta Dutta",
+    author_email="sumanta.dutta2012@gmail.com",
+    description="ABN Amro technical assignment package",
+)
