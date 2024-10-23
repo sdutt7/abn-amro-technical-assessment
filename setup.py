eb6c73e128ea5266d4d202fa91f45c405fa0e872
sdutt7 <sumanta.dutta2012@gmail.com>
2024-10-24 01:00:40 +0200

    Feature/it data (#5)
    * CSV Reader Module Integrated
    
    * Transform Module Integrated
    
    * Writer Module Integrated
    
    * Option2 transformation integrated
    
    * Option2 transformation integrated
    
    * Option2 transformation integrated
    
    * Option3 transformation integrated
    
    * Option4 transformation integrated
    
    * Option4 transformation integrated
    
    * Option6 transformation integrated
    
    * Fixed bug
    
    * Fixed bug
    
    * Fixed bug
    
    * Transform7 Integrated
    
    * Transform7 Integrated
    
    * Transform8 Integrated
    
    * Job created
    
    * Job created
    
    * Job created
    Output file added
    
    * transform1 test case added
    
    * transform2 test case added
    
    * transform2 test case added
    
    * transform3 test case added
    
    * transform3 test case added
    
    * transform4 test case added
    
    * transform5 test case added
    
    * transform6 test case added
    
    * Docstring added
    
    * Docstring added
    
    * Output files committed
    
    * Commit setup.py
    
    * Commit setup.py
    
    * Commit setup.py
    
    * Commit setup.py
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    * Commit python-app.yml
    
    * Added test cases
    
    * Added test cases
    
    * Updated Release Version
    
    ---------
    
    Co-authored-by: sumanta dutta <pminstu@imptobnet.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="abn-amro-assessment-2024",
-    version="0.0.1",
+    version="0.0.5",
     packages=[
         "framework",
         "framework.base",
