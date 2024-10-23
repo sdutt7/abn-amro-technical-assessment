0437e68f55322d457dfe0c0d7c3ac2701847ee8f
sumanta dutta <pminstu@imptobnet.com>
2024-10-23 23:40:10 +0200

    Commit python-app.yml

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 with open("requirements.txt") as f:
     required = f.read().splitlines()
 
@@ -18,7 +21,7 @@ setup(
     install_requires=required,
     author="Sumanta Dutta",
     author_email="sumanta.dutta2012@gmail.com",
-    long_description="ABN Amro technical assignment package",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     description="ABN Amro technical assignment package",
 )
